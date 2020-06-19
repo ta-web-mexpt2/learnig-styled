ae59b200c398262f237c1ac06e20b15de40f151a
Dylan Torres Cruz <dylan@programmer.city>
2020-06-18 19:44:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+const  AppWrapper = styled.div`
+ text-align:center;
+`
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+const AppLogo = styled.img`
+  animation: ${rotate360} infinite 10s linear;
+  height: 80px;
+  &:hover{
+    animation:${rotate360} infinite 1s linear;
+  }
+`
+
+const AppHeader = styled.div`
+  background-color: pink;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`
+const AppTitle = styled.h1`
+  font-size: 1.3em;
+`
+const AppIntro = styled.p`
+  font-size: large;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+
+      <AppHeader>
+        <AppLogo src={logo}  alt="logo" />
+        <AppTitle>
+          Hello Ironhacker
+        </AppTitle>
+      </AppHeader>
+
+      <AppIntro>
+        Esto es con styled component
+      </AppIntro>
+
+    </AppWrapper>
   );
 }
 
